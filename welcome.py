46ed03ccb853ef967582f29d159678a4555f8519
Kushieeee <159754661+Kushieeee@users.noreply.github.com>
2024-02-21 12:53:26 +0300

    Add files via upload

---
+++ welcome.py
@@ -0,0 +1,37 @@
+print("My name is Kush")
+greeting ="Hello"
+name = "John"
+location = "Juja"
+pet = "cat" #string
+age = 80 #integer
+print(greeting)
+print (name)
+print (location)
+#this is a comment
+height =2.3 
+print (height)
+#datatypes inc strings,intergers,floats,dictionaries
+#variables are data containers...can contain any data type
+#python keywords; words natively known to python 
+    #eg print()
+#rules for naming variables:
+"""self definitive
+    must not have spaces and dashes
+    cannot be a number or python keyword"""
+#first name = "max"
+#error is space; no space or underscore
+firstname = "max"
+#3=80 # can't be or begin with a number
+#STRINGS MUST ALWAYS BE ENCLOSED
+#= for declaring
+x=40
+y=4
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+x,y,z,u =10,4,6,9 #mass assignment
+print(x)
+first_name = input("Enter name: ")
+last_name = input("Enter your last name: ")
+print(first_name + last_name) 
