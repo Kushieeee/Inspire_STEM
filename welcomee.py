ef63020ff412b4158924a36553385dcd7edb898e
Kushieeee <159754661+Kushieeee@users.noreply.github.com>
2024-02-21 14:11:06 +0300

    Add files via upload

--- welcomee.py
+++ welcomee.py
@@ -1,3 +1,26 @@
 firstname="Joy "
 lastname= "Kuria"
-print(firstname + lastname)
+#print(firstname + lastname)
+#print(firstname,lastname)
+firstnumber=7
+secondnumber=30
+sum= firstnumber+secondnumber
+#print(sum)
+sub= firstnumber-secondnumber
+#print(sub)
+print("Finding the area of a circle")
+r= int(input("Enter the radius"))
+pi=22/7#where pi is π
+area= pi*r*r
+output="The area is"
+print(output,area)
+a=3
+b=7
+h=2
+area=(a+b)/2*h
+output="The area is"
+#print(output,area)
+#name= input("Enter first name")
+#print(name)
+
+
